053f737d01c8a3f29bf7df96f44666ec070b0e92
pedroporo1000 <pedroguillferri9@gmail.com>
2025-03-26 09:18:02 +0100

    Remove slack stuff

--- callbot.py
+++ callbot.py
@@ -60,31 +60,12 @@ def validate_phone_numbers(phone_numbers):
     return True, None
 
 
-def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is a firehose of data, so
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip(), \
-                       output['channel']
-    return None, None
 
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
+    while True:
         print("CallBot connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+        time.sleep(READ_WEBSOCKET_DELAY)
+
 
